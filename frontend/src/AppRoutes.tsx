d6515731e642a05689d02bd2da9098bc722c7927
deepaksharmadk <dks847203@gmail.com>
2024-03-30 00:37:28 +0530

    initiate router setup

---
+++ frontend/src/AppRoutes.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={"hello"}></Route>
+    </Routes>
+  );
+}
+
+export default AppRoutes;
