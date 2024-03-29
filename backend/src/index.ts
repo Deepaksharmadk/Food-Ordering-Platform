ab553b75a9426923fa7ed767f91a713c66d8ce41
deepaksharmadk <dks847203@gmail.com>
2024-03-30 00:20:14 +0530

    connect database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,13 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
+  console.log("Connected to MongoDB");
+});
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 app.get("/", (req: Request, res: Response) => {
