c22768505f3b8dfe393e9dd8f0723b8ffb2c552c
deepaksharmadk <dks847203@gmail.com>
2024-05-16 21:52:53 +0530

    added home page

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,6 +1,10 @@
 import hero from "../assets/hero.png";
 function Hero() {
-  return <img src={hero} className="w-full max-h-[600px] object-cover" />;
+  return (
+    <>
+      <img src={hero} className="w-full max-h-[600px] object-cover" />;
+    </>
+  );
 }
 
 export default Hero;
