b353a3a39b9b3a83656127c26e49167d85192954
deepaksharmadk <dks847203@gmail.com>
2024-05-16 21:15:03 +0530

    mainNAv component

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import MobileNav from "./MobileNav";
+import MainNav from "./MainNav";
 function Header() {
   return (
     <div className="border-b-2 border-b-orange-500 py-6">
@@ -10,8 +11,11 @@ function Header() {
         >
           Deepak.com
         </Link>
-        <div className="">
+        <div className="md:hidden">
           <MobileNav />
+        </div>
+        <div>
+          <MainNav />
         </div>
       </div>
     </div>
