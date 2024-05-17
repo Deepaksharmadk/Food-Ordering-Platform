f7019de51e4bf479863627f520c7186dfb14b317
deepaksharmadk <dks847203@gmail.com>
2024-05-17 22:19:36 +0530

    provided to main.js auth

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,9 +1,17 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./Layouts/Layout";
+import HomePage from "./pages/HomePage";
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Layout>H</Layout>}></Route>
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <HomePage />
+          </Layout>
+        }
+      ></Route>
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
